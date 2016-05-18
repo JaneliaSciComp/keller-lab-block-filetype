e35f9751e6f989d90238b9858faede018a65be7e
Burkhard Hoeckendorf <b.hoeckendorf@web.de>
2016-05-18 13:09:05 -0400

    Add test for writing through JNI

--- javaWrapper/src/test/java/org/janelia/simview/klb/KlbTest.java
+++ javaWrapper/src/test/java/org/janelia/simview/klb/KlbTest.java
@@ -18,6 +18,7 @@ public class KlbTest
     private final KLB klb = KLB.newInstance();
     private final String testReadFilePath = new File( "../testData/img.klb" ).getAbsolutePath();
     private final String testCellImgReadFilePath = new File( "src/test/resources/cellimg.klb" ).getAbsolutePath();
+    private final String testWriteFilePath = new File( "src/test/resources/deleteme.klb" ).getAbsolutePath();
 
     @Test
     public void readHeader()
@@ -158,5 +159,61 @@ public class KlbTest
         assertEquals( 1027, Math.round( mean ) );
 
         assertEquals( 7, Math.round( (( RealType ) img.firstElement()).getRealDouble() ) );
+    }
+
+    @Test
+    public void write()
+    {
+        if ( new File( testWriteFilePath ).exists() ) {
+            new File( testWriteFilePath ).delete();
+        }
+
+        ImgPlus img = null;
+        try {
+            img = klb.readFull( testReadFilePath );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        assertNotNull( img );
+        assertTrue( img.getImg() instanceof ArrayImg );
+        assertEquals( 3, img.numDimensions() );
+
+        try {
+            klb.writeFull( img, testWriteFilePath, null, KLB.CompressionType.BZIP2, null );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        assertTrue( new File( testWriteFilePath ).exists() );
+
+        ImgPlus img2 = null;
+        try {
+            img2 = klb.readFull( testWriteFilePath );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        assertNotNull( img2 );
+        assertTrue( img2.getImg() instanceof ArrayImg );
+        assertEquals( 3, img2.numDimensions() );
+
+        final long[] dims = new long[ 3 ], dims2 = new long[ 3 ];
+        img.dimensions( dims );
+        img2.dimensions( dims2 );
+        assertArrayEquals( dims, dims2 );
+
+        double mean = 0, mean2 = 0;
+        long n = 1;
+        for ( final long d : dims ) {
+            n *= d;
+        }
+        final Cursor< ? extends RealType< ? > > cur = img.cursor(), cur2 = img2.cursor();
+        while ( cur.hasNext() ) {
+            mean += cur.next().getRealDouble() / n;
+            mean2 += cur2.next().getRealDouble() / n;
+        }
+        assertEquals( Math.round( mean ), Math.round( mean2 ) );
+
+        if ( new File( testWriteFilePath ).exists() ) {
+            new File( testWriteFilePath ).delete();
+        }
     }
 }
