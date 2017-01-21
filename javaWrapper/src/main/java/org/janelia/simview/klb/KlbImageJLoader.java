d020c149c8779ae435afc563efe8910dc66f6c32
Burkhard Hoeckendorf <b.hoeckendorf@web.de>
2017-01-21 15:05:21 -0500

    KlbImageJLoader reorders dimensions if needed and uses UIService

--- javaWrapper/src/main/java/org/janelia/simview/klb/KlbImageJLoader.java
+++ javaWrapper/src/main/java/org/janelia/simview/klb/KlbImageJLoader.java
@@ -3,13 +3,18 @@ package org.janelia.simview.klb;
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
 import net.imagej.ImgPlus;
+import net.imagej.axis.Axes;
+import net.imagej.axis.Axis;
+import net.imagej.axis.AxisType;
+import net.imagej.axis.CalibratedAxis;
+import net.imglib2.view.Views;
 import org.scijava.command.Command;
-import org.scijava.display.DisplayService;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+import org.scijava.widget.FileWidget;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import java.io.File;
 import java.io.IOException;
 
@@ -22,54 +27,59 @@ public class KlbImageJLoader implements Command
     private DatasetService datasetService;
 
     @Parameter
-    private DisplayService displayService;
+    private UIService uiService;
+
+    @Parameter
+    private LogService logService;
 
     @Override
     public void run()
     {
+        final File file = uiService.chooseFile( null, FileWidget.OPEN_STYLE );
+        if ( file == null )
+            return;
+
+        if ( klb == null )
+            klb = KLB.newInstance();
+
+        ImgPlus img;
         try {
-            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
-        } catch ( ClassNotFoundException e ) {
-            e.printStackTrace();
-        } catch ( InstantiationException e ) {
-            e.printStackTrace();
-        } catch ( IllegalAccessException e ) {
-            e.printStackTrace();
-        } catch ( UnsupportedLookAndFeelException e ) {
-            e.printStackTrace();
-        }
-
-        final JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle( "Import KLB file" );
-        fileChooser.setFileFilter( new FileFilter()
-        {
-            @Override
-            public boolean accept( final File f )
-            {
-                return f.isDirectory() || f.getName().toLowerCase().endsWith( ".klb" );
-            }
-
-            @Override
-            public String getDescription()
-            {
-                return "KLB files";
-            }
-        } );
-        if ( fileChooser.showOpenDialog( null ) != JFileChooser.APPROVE_OPTION ) {
+            img = klb.readFull( file.getAbsolutePath() );
+        } catch ( IOException e ) {
+            logService.error( e );
             return;
         }
 
-        if ( klb == null ) {
-            klb = KLB.newInstance();
+        Dataset data;
+        // ImageJ expects xyczt order, loaded order is xyzct.
+        // Permute if needed, be mindful of squeezed singleton dimensions.
+        int zDim = 2, cDim = -1;
+        for ( int i = 0; i < img.numDimensions(); ++i ) {
+            final Axis ax = img.axis( i );
+            if ( ax instanceof CalibratedAxis ) {
+                final AxisType axType = (( CalibratedAxis ) ax).type();
+                if ( axType == Axes.CHANNEL ) {
+                    cDim = i;
+                    break;
+                }
+            }
+        }
+        if ( cDim != -1 && cDim != 2 ) {
+            data = datasetService.create( Views.permute( img, zDim, cDim ) );
+            final CalibratedAxis[] axes = new CalibratedAxis[ img.numDimensions() ];
+            for ( int i = 0; i < axes.length; ++i ) {
+                if ( i == zDim )
+                    axes[ i ] = ( CalibratedAxis ) img.axis( cDim );
+                else if ( i == cDim )
+                    axes[ i ] = ( CalibratedAxis ) img.axis( zDim );
+                else
+                    axes[ i ] = ( CalibratedAxis ) img.axis( i );
+            }
+            data.setAxes( axes );
+        } else {
+            data = datasetService.create( img );
         }
 
-        final String file = fileChooser.getSelectedFile().getAbsolutePath();
-        try {
-            final ImgPlus img = klb.readFull( file );
-            final Dataset data = datasetService.create( img );
-            displayService.createDisplay( data );
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        }
+        uiService.show( file.getName(), data );
     }
 }
