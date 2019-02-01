ba5935110ec729887b674348d5cb005b10046f5c
Stuart Berg <bergs@janelia.hhmi.org>
2019-02-01 17:48:09 -0500

    conda-recipe: On Linux, use Anaconda's newer gcc package

--- src/conda-recipe/build.sh
+++ src/conda-recipe/build.sh
@@ -2,15 +2,25 @@ cd src/
 mkdir build
 cd build
 
-if [[ `uname` == 'Darwin' ]]; then
+# Depending on our platform, shared libraries end with either .so or .dylib
+if [[ $(uname) == 'Darwin' ]]; then
     DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
+    CXXFLAGS="-I${PREFIX}/include -stdlib=libc++ -std=c++11"
 else
     DYLIB_EXT=so
+    CXXFLAGS="-I${PREFIX}/include"
+
+    # Don't specify these -- let conda-build do it.
+    #CC=gcc
+    #CXX=g++
 fi
 
 cmake .. \
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
 \
